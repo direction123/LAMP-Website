bde32ddfe435008a5dd5405c5cd6bdbee9e4c48d
direction123 <direction.wfx@gmail.com>
2016-11-16 12:40:01 -0800

    basic url

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = 'https://stark-cove-98897.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
