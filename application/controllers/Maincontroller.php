d60004435c9bc15cb091850d8a28bfe68a11b4ab
direction123 <direction.wfx@gmail.com>
2016-11-16 17:26:49 -0800

    db

--- application/controllers/Maincontroller.php
+++ application/controllers/Maincontroller.php
@@ -1,12 +1,19 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Maincontroller extends CI_Controller {
+class Maincontroller extends CI_Controller 
+{
+    public function __construct()
+    {
+        parent::__construct();
+// Load database
+        $this->load->model('Maindatabasemodel');
+    }
 
-  public function index()
-  {
-  	$para['ProductCategoryDetails']=$this->Maindatabasemodel->getProductCategory();
-    $this->load->view('HeaderGuestView');
-  }
+    public function index()
+    {
+  	    $para['ProductCategoryDetails']=$this->Maindatabasemodel->getProductCategory();
+        $this->load->view('HeaderGuestView');
+    }
 }
 
 ?>
