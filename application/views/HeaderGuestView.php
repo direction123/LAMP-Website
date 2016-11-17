d3e8c45b163606c4e7bf39405229453906107631
direction123 <direction.wfx@gmail.com>
2016-11-16 17:19:13 -0800

    asset

--- application/views/HeaderGuestView.php
+++ application/views/HeaderGuestView.php
@@ -3,6 +3,8 @@
 <head>
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script src="<?php echo asset_url(); ?>/style.js"></script>
+    <link rel="stylesheet" type="text/css" href="<?php echo asset_url(); ?>/mystyle.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
 <div id="header">
