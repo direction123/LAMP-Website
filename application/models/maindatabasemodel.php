d72c9f80d2c65c197530e9d836f71606f15badde
direction123 <direction.wfx@gmail.com>
2016-11-16 18:25:38 -0800

    db

--- application/models/maindatabasemodel.php
+++ application/models/maindatabasemodel.php
@@ -1,4 +1,4 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php 
 
 class maindatabasemodel extends CI_Model {
 	public function __construct()
@@ -16,3 +16,4 @@ class maindatabasemodel extends CI_Model {
 
 /* End of file maindatabasemodel.php */
 /* Location: ./application/models/maindatabasemodel.php */
+?>
