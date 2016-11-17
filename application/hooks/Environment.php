a6d1b6014151e652df789a31dd74ef0cefa5650a
direction123 <direction.wfx@gmail.com>
2016-11-16 17:55:52 -0800

    db

--- application/hooks/Environment.php
+++ application/hooks/Environment.php
@@ -3,7 +3,7 @@
 /**
  * Ensure that environment variables are set
  */
-// function check_environment() {
+function check_environment() {
 //     if (!(getenv("DATABASE_URL")))
 //         throw new Exception('Must set a DATABASE_URL environment variable');
-// }
+}
