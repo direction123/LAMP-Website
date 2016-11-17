a3219c138dceecebe0c0156792eba6c0f5e7504f
direction123 <direction.wfx@gmail.com>
2016-11-16 17:04:45 -0800

    add codes

---
+++ application/views/CartEmptyView.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="<?php echo asset_url(); ?>/style.js"></script>
+    <link rel="stylesheet" type="text/css" href="<?php echo asset_url(); ?>/mystyle.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+<div class="grid">
+    <div class="row">
+        <div class="col-2">
+        </div>
+
+        <div class="col-10">
+            <h2>Shopping Cart</h2>
+            <div class="cartProd">Product</div>
+            <div class="cartPrice">Price</div>
+            <div class="cartQuant">Quantity</div>
+            <div class="cartEmpty">Shopping cart is empty</div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
---
+++ application/views/CusOrderView.php
@@ -0,0 +1,35 @@
+<html>
+<head>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="<?php echo asset_url(); ?>/style.js"></script>
+    <link rel="stylesheet" type="text/css" href="<?php echo asset_url(); ?>/mystyle.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+<div class="grid">
+    <div class="row">
+        <div class="col-2">
+        </div>
+
+        <div class="col-10">
+            <?php
+            echo '<h2>Order List</h2>';
+            foreach ($customerOrder as $cO) {
+                echo '<form action="/index.php/Maincontroller/view_cus_order_details" method="post">';
+                echo '<div class="orderList">';
+                    echo '<div class="orderListDate">';
+                        echo '<span>'.$cO->orderDate.'</span>';
+                        echo '<input type="hidden" name="orderID" value="' . $cO->orderID. '">';
+                    echo '</div>';
+                    echo '<div class="orderListBT">';
+                        echo '<input type="submit" name="submit" class="borderInput" value="Details">';
+                    echo '</div>';
+                echo '</div>';
+                echo "</form>";
+            }
+            ?>
+        </div>
+    </div>
+</div>
+</body>
+</html>
