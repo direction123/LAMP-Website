174e86e5cd1a4f6db0ff87d5e105f8356604a729
direction123 <direction.wfx@gmail.com>
2016-11-16 17:17:16 -0800

    asset url

--- application/views/HeaderGuestView.php
+++ application/views/HeaderGuestView.php
@@ -1,7 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
-   </head>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="<?php echo asset_url(); ?>/style.js"></script>
+</head>
 <body>
 <div id="header">
     test
